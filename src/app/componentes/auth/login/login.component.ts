e08ecf992756cd79703c7d5c3c13b5ac13b0473e
JavierRamosVillarejo <javierramosvillarejo@gmail.com>
2021-06-19 18:28:39 +0200

    login y registro acabados

--- src/app/componentes/auth/login/login.component.ts
+++ src/app/componentes/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/servicios/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin= this.fb.group({
+    password:['', [Validators.required]],
+    email:['', [Validators.required, Validators.email]]
+  })
+  mensaje: string=""
+
+  constructor(private fb: FormBuilder, private servicioUsuario: UserService, private irHacia: Router) { }
 
   ngOnInit(): void {
+
+    if(this.servicioUsuario.isLoged()){
+      this.irHacia.navigate(["/perfil"])
+    }
+
+  }
+  submit():void {
+    this.servicioUsuario.acceso(this.formLogin.value).subscribe(
+      respuesta =>{
+        console.log(respuesta)
+        this.servicioUsuario.guardarToken(respuesta)
+        this.irHacia.navigate(["/perfil"])
+
+      },
+      error => {
+        console.log(error)
+      this.mensaje= error.error.error }
+    )
+
   }
 
 }
